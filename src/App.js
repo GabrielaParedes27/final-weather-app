33708b491812b4f3acdaa11227125b9d1265201e
GabrielaParedes27 <78928248+GabrielaParedes27@users.noreply.github.com>
2021-04-21 19:51:52 -0500

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather app
+      <div className="container">
+     <h1>Weather app</h1> 
+     <footer>
+       This project was coded by{" "}
+       <a href="www.linkedin.com/in/gabriela-paredes-314333180" target="_blank">
+         Gabriela Paredes
+       </a>{" "}
+       and is{" "}
+       <a href="https://github.com/GabrielaParedes27/final-weather-app.git" target="_blank">
+         open-sourced on GitHub
+       </a>
+     </footer>
+     </div>
     </div>
   );
 }
